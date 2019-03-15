57512df1e2aaa4d64220de8e44fff54a5c598465
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-15 13:30:41 +0100

    added doc comments

--- src/main/java/com/onlinestore/eurekaservice/EurekaServerApplication.java
+++ src/main/java/com/onlinestore/eurekaservice/EurekaServerApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+/**
+ * Class that bootstraps this service.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+	/**
+	 * Main method, used to run this application.
+	 *
+	 * @param args the string array, that contains command line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
